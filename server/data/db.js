e241e3f246759edf50aad4c04a6a922b5e839af9
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-06-25 15:23:48 +0300

    added mongoose trycatch

--- server/data/db.js
+++ server/data/db.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const url = process.env.MONGO_URL;
-
-mongoose.connect(url, { useNewUrlParser: true });
+try {
+  mongoose.connect(url, { useNewUrlParser: true }, (client, err) => {
+    if (client) console.log("mongoose connected");
+  });
+} catch (error) {
+  console.log("mongoose failed");
+}
