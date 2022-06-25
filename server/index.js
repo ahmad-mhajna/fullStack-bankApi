18d2d9b4a0b4dbecc56d9011039f764eb193b38f
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-06-25 15:08:39 +0300

    added express static

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("./data/db");
+const path = require("path");
 const express = require("express");
 const AccountsRouter = require("./routes/accounts");
 const server = express();
@@ -8,7 +9,7 @@ const cors = require("cors");
 server.use(express.json());
 server.use(cors());
 server.use("/api", AccountsRouter);
-
+server.use(express.static(path.resolve(__dirname, "../client/build")));
 server.listen(port, () => {
   console.log("server is up " + port);
 });
